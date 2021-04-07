57cd4f783f9c828ef70023c9a5df03f1752abdcf
Hikmat Samadov <hikmetsemedov0177@gmail.com>
2021-04-07 10:10:52 +0400

    Updated to Django 3.2

--- app/customer/apps.py
+++ app/customer/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class CustomerConfig(AppConfig):
-    name = 'customer'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app.customer'
--- app/shopping/apps.py
+++ app/shopping/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ShoppingConfig(AppConfig):
-    name = 'shopping'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app.shopping'
